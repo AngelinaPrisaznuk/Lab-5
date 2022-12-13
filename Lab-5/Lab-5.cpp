21745575b6c1d44321322bbed0d5b3a0a77ea487
AngelinaPrisaznuk <114239162+AngelinaPrisaznuk@users.noreply.github.com>
2022-12-13 21:53:00 +0200

    Add project files.

---
+++ Lab-5/Lab-5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	short m[] = { 4, 7, 8, 6, 66, 5, 9, 2, 1, 22, 12 };
+	int x, y;
+	cout << "x";
+	cin >> x;
+	cout << "y";
+	cin "y";
+	kilk = 0;
+	for (int i = 0; i < 11; i++)
+		if (m[i] = x || m[i] = y);
+	fout << kilk << endl;
+}
